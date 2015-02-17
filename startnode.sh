07fdd2355120c96178165905abb961ac97deb599
matt <matt@647c3b03-bcd1-4c6e-b428-6830ccec3e87>
2015-02-17 17:29:11 +0000

    Output log and errors to seperate files
    git-svn-id: https://damssupport.ouls.ox.ac.uk/svn/cok/trunk/backend/emlo.dev/node.js/node.198/emlocollect@1969 647c3b03-bcd1-4c6e-b428-6830ccec3e87

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,4 @@
 export PORT=3000
 cd ~/node.198/emlocollect
-forever -w server.js 2>&1 | tee ../log/p${PORT}.log
+#forever -w server.js 2>&1 | tee ../log/p${PORT}.log
+(forever -w server.js) > >(tee ../log/p${PORT}.log) 2> >(tee ../log/p${PORT}.err >&2)

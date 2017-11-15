702257496f8c23c9c1c308bf90ce4b986acd61e0
Emlo-Edit Server <matthew.wilcoxson@bodleian.ox.ac.uk>
2017-11-15 16:49:03 +0000

    Stop outputting logs

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,5 @@
 export PORT=3000
 cd /home/cofkadmin/backend/emlo.dev/node.js/node.198/git/
-(forever -w server.js) > >(tee log/p${PORT}.log) 2> >(tee log/p${PORT}.err >&2)
+MESSAGE="\n\n--------------------------------------------\n  SERVER RESTART $(date)  \n--------------------------------------------------\n" 
+printf "$MESSAGE" >> log/p${PORT}.err
+forever -a -l "/dev/null" -o "/dev/null" -e "log/p${PORT}.err" start server.js

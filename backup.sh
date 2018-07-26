c5921c6ab03e41deaeaf06c7655a113dde83f835
Matthew Wilcoxson <git@akademy.co.uk>
2018-07-26 16:29:35 +0100

    Backup scripts.

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /data/emlo-editor
+. backup-helper.sh
+
+DEST=/data/backups
+FILENAME=mongo-dump_emlo-edit.tar.gz
+
+docker-compose exec mongo sh -c 'mongodump -d emlo-edit -o /data/db/dump'
+tar -czf $DEST/$FILENAME -C volumes/mongo/ dump
+
+backup_rotate_store $DEST $FILENAME

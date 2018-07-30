442b6ae98ff5d3886fdd4359cfae5d4a8afeecf7
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-07-30 12:50:38 +0100

    Back up tweaks.

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ DEST=/data/backups
 FILENAME=mongo-dump_emlo-edit.tar.gz
 
 docker-compose exec mongo sh -c 'mongodump -d emlo-edit -o /data/db/dump'
-tar -czf $DEST/$FILENAME -C volumes/mongo/ dump
+tar -czf ${DEST}/${FILENAME} -C volumes/mongo/ dump
 
-backup_rotate_store $DEST $FILENAME
+backup_rotate_store ${DEST} ${FILENAME}

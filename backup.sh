a944243cdfc63bb3a3eb3c161d677cae092f79e3
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-08-09 18:30:00 +0100

    Backup improvements. Use remote repo for helper.

--- backup.sh
+++ backup.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-cd /data/emlo-editor
-. backup-helper.sh
+# An example backup script using backup-helper. To be run daily.
+destination=/data/backups
+filename=my-backup.tar.gz
 
-DEST=/data/backups
-FILENAME=mongo-dump_emlo-edit.tar.gz
+cd /data/emlo-collect
+
+# First include the backup script
+backup_helper=backup-script/backup-helper.sh
+
+if [ ! -f ${backup_helper} ]; then
+    echo "The backup-helper.sh file is required. Not found at ${backup_helper}. Download it at https://bitbucket.org/akademy/backup-script"
+	exit 1
+fi
+
+. ${backup_helper}
+
+now=$(date)
+
+echo "Backing up at ${now} to ${destination}/${filename} ..."
 
 docker-compose exec mongo sh -c 'mongodump -d emlo-edit -o /data/db/dump'
-tar -czf ${DEST}/${FILENAME} -C volumes/mongo/ dump
+tar -czf ${destination}/${filename} -C volumes/mongo/ dump
 
-backup_rotate_store ${DEST} ${FILENAME}
+backup_rotate_store ${destination} ${filename}
+
+now=$(date)
+echo "... ${now} backup complete."
